7014731dd6870dc7d9db3eefdc2e191cbe125688
Vedad Yornic <vedadsa@windowslive.com>
2014-11-18 15:46:01 +0100

    Zadaca

---
+++ StringRazlicitiZnakovi.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/** Provjeriti dali unesena rijec ima duplih slova
+ * 
+ * @author vedadzornic
+ *
+ */
+public class StringRazlicitiZnakovi {
+public static void main(String[] args) {
+	String unos;
+		
+	System.out.println("Unesite jednu rijec ");
+	Scanner in = new Scanner(System.in);
+	unos = in.next();
+	
+	for(int i=0; i<(unos.length()); i++){
+	    
+            if(unos.contains(unos.charAt(i))) {
+            	
+            	
+}
+	}
+            
+}
+}
